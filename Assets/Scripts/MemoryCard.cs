2d3d8aa63a1fbeefe905d33e048c6fa76eda6ac1
TimYalovoy <49865661+TimYalovoy@users.noreply.github.com>
2019-04-22 15:27:49 +0400

    Add files via upload

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardback;
+    [SerializeField] private SceneController controller;
+
+    private int _id;
+    public int id
+    {
+        get { //функция чтения(идиома)
+            return _id;
+        }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void OnMouseDown()
+    {
+        if (cardback.activeSelf || controller.canReveal)
+        {
+            cardback.SetActive(false);
+            controller.CardRevealed(this);
+        }
+    }
+
+    public void Unreveal()
+    {
+        cardback.SetActive(true);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
